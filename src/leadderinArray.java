ebe50e8e8553e1ce31dcd33f699aa3aa1559689b
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-07-05 10:21:42 +0530

    Update leadderinArray.java

--- src/leadderinArray.java
+++ src/leadderinArray.java
@@ -1,26 +1,20 @@
 import java.util.Scanner;
-
-public class leaderinArray {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-         int leader ;
-        int[] array = new int[n];
-        for (int i = 0; i<n; i++){
-            array[i] =  sc.nextInt();
-        }
-        leader = array[n-1];
-        System.out.println(leader);
-        for (int i = n-1; i>0;i--){
-            if (array[i]>array[i-1]){
-                leader = array[i];
-                System.out.println(leader);
-            }else {
-                leader = array[i-1];
-                System.out.println(leader);
+public class leader {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int leader ;
+            int[] array = new int[n];
+            for (int i = 0; i<n; i++){
+                array[i] =  sc.nextInt();
             }
-
+            leader = array[n-1];
+            System.out.println(leader);
+            for (int i = n-1; i>0;i--){
+                if (leader<array[i-1]){
+                    leader = array[i-1];
+                    System.out.print(leader +", ");
+                }
+            }
         }
-
     }
-}
