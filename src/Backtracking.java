21a263f63942c145e9ca8bdf0e069c6e9e08172b
Priyansh <79632896+Priyansh521@users.noreply.github.com>
2023-07-15 21:22:06 +0530

    Create Backtracking.java

---
+++ src/Backtracking.java
@@ -0,0 +1,17 @@
+public class backtracking {
+    public static void printPermutation(String x, int idx, String perm){
+        if (x.length()== 0){
+            System.out.println(perm);
+            return;
+        }
+        for (int i = 0; i<x.length(); i++){
+            char currChar = x.charAt(i);
+            String newStr = x.substring(0,i) + x.substring(i+1);
+            printPermutation(newStr, idx+1, perm+currChar);
+        }
+    }
+    public static void main(String[] args) {
+              String x = "abc";
+              printPermutation(x, 0, "");
+    }
+}
